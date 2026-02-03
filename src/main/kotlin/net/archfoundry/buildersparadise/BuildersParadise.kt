2f9fa1779ad6a743090ce4fea0b3ca9f4435c1eb
AshTheRedPanda <ashi.workemail@gmail.com>
2026-02-03 19:25:49 +0200

    init kotlin attempt

--- src/main/kotlin/net/archfoundry/buildersparadise/BuildersParadise.kt
+++ src/main/kotlin/net/archfoundry/buildersparadise/BuildersParadise.kt
@@ -1,11 +1,15 @@
 package net.archfoundry.buildersparadise
 
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.eventbus.api.IEventBus
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Mod(BuildersParadise.MOD_ID)
-class BuildersParadise {
+class BuildersParadise(modBus: IEventBus) {
 
     companion object {
         const val MOD_ID = "buildersparadise"
@@ -13,6 +17,21 @@ class BuildersParadise {
     }
 
     init {
+        val modBus = FMLJavaModLoadingContext.get().modEventBus
+
+        modBus.addListener<FMLCommonSetupEvent> { event ->
+            this.commonSetup(event)
+        }
+
+        // RegistryModule.BLOCKS.register(modBus)
+        // RegistryModule.ITEMS.register(modBus)
+
+        MinecraftForge.EVENT_BUS.register(this)
+
         LOGGER.info("Builders Paradise (Kotlin) loaded successfully!")
+    }
+
+    private fun commonSetup(event: FMLCommonSetupEvent) {
+
     }
 }
